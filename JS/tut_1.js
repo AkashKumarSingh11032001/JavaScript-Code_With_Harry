92edc1cb3e283dbd05a095be206a96540df12107
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-03 21:24:00 +0530

    Day3

---
+++ JS/tut_1.js
@@ -0,0 +1,10 @@
+console.time('start');
+console.log("Hello World");
+console.log(true);
+console.log([5,3,9,7,5]);
+console.log({harry: 'this',marks:65});
+console.table({harry: 'this',marks:65});
+//console.warn('Warning mssg');
+console.timeEnd("End at");
+// console.clear();
+//console.error('this is error')
