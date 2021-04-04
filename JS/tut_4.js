4f63294f688c8784ffb88e452099aa52249ecee6
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-04 10:35:06 +0530

    Day4

---
+++ JS/tut_4.js
@@ -0,0 +1,58 @@
+//  six primitive types like Boolean, number, string, undefined, null, symbol.
+//  reference type as object literals, array, functions, and dates.
+
+// Primetive DataType
+
+// String:
+var name = "Akash";
+console.log(name);
+console.log(typeof name); // Datatype type
+
+// NUmber:
+let num = 34;
+console.log(num);
+console.log(typeof num);
+
+// Boolean:
+let ismale = true;
+console.log(ismale);
+console.log(typeof ismale);
+
+// Null
+let nulVar = null;
+console.log(nulVar);
+console.log(typeof nulVar);
+
+// Undefined:
+let undefVar = undefined;
+console.log(undefVar);
+console.log(typeof undefVar);
+
+// Refrence DataType
+
+// Arrays
+let myarr = [2,35,8,9,14,6];
+console.log( typeof myarr); // obj
+
+// Object
+let StMarks= {
+
+    harry: 89,
+    
+    Shubham: 36,
+    
+    Rohandas: 34
+}
+console.log( typeof StMarks);
+
+// Function
+function findName() {
+    
+}
+console.log( typeof findName);
+
+// Date
+let date = new Date();
+console.log( typeof date);
+
+
