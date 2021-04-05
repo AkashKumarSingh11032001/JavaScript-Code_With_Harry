5a5fed7ce92bbf21f7eb25d5b07bf92074c30057
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-05 23:28:28 +0530

    Day5

--- JS/app.js
+++ JS/app.js
@@ -1,39 +1,9 @@
 
-const marks = [34,50,26,55,95,21,44,48];
-const fruits = ['a','b','c','d','e'];
-const mixed= ['str',89,[2,6]];
-
-const arr = new Array(23,25,21,'orange');
-
-console.log(marks);
-console.log(fruits);
-console.log(mixed);
-console.log(arr);
-
-// let age = [33,55,67,18]
-// console.log(age.length)
-
-// let std = ["Mark", "John", "Jonas", "Jack"];
-// let a = std.indexOf("John");
-
-// let age1 = [33,55,67,18]
-// let s_age= age.sort(age1)
-
-// let fruits1 = ["Banana", "Orange", "Apple"];
-// let len = fruits1.push('Mango')
-// let last = fruits1.pop()
-
-// let removedItem = fruits1.splice(pos, 1) // this is how to remove an item
-// // ["Strawberry", "Orange"]
-
-// O B J E C T 
-
-let myobj = {
-    'first name': 'harry', 
-    channel: 'CodeWithHarry',
-    isActive: true,
-    marks: [1,5,3,6]
+let salary = 500;
+if( salary > 1000)
+{
+console.log("My Salary is greater than 1000");
+}else{
+console.log("My Salary is less than or equal to 1000");
 }
-console.log(myobj)
-console.log(myobj['channel'])
-console.log(myobj.channel)
+
