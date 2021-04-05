dd79edb6dd85b1ec5e694599e5cd878aaeb0e08c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-05 23:29:07 +0530

    Day5

--- JS/app.js
+++ JS/app.js
@@ -7,13 +7,15 @@
 // console.log("My Salary is less than or equal to 1000");
 // }
 
-var salary = 500;
-if( salary > 1000)
-{
-console.log("My Salary is greater than 1000");
-}
-else if(salary < 1000){
-console.log("My Salary is less than 1000");
-} else{
-console.log("My Salary equal to 1000");
-}
+// var salary = 500;
+// if( salary > 1000)
+// {
+// console.log("My Salary is greater than 1000");
+// }
+// else if(salary < 1000){
+// console.log("My Salary is less than 1000");
+// } else{
+// console.log("My Salary equal to 1000");
+// }
+
+5>0? "Greater" : "Smaller";
