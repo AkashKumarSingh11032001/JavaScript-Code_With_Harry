ee3dc5beebac52ceec59fb4a3ca88e42110e934d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-05 23:21:49 +0530

    Day5

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,19 @@ console.log(marks);
 console.log(fruits);
 console.log(mixed);
 console.log(arr);
+
+let age = [33,55,67,18]
+console.log(age.length)
+
+let std = ["Mark", "John", "Jonas", "Jack"];
+let a = std.indexOf("John");
+
+let age1 = [33,55,67,18]
+let s_age= age.sort(age1)
+
+let fruits1 = ["Banana", "Orange", "Apple"];
+let len = fruits1.push('Mango')
+let last = fruits1.pop()
+
+let removedItem = fruits1.splice(pos, 1) // this is how to remove an item
+// ["Strawberry", "Orange"]
