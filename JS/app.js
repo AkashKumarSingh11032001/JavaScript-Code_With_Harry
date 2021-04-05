35f18b82346d2d3e74b916af6928d6d226fe365a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-05 23:11:47 +0530

    Day5

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+
+const marks = [34,50,26,55,95,21,44,48];
+const fruits = ['a','b','c','d','e'];
+const mixed= ['str',89,[2,6]];
+
+const arr = new Array(23,25,21,'orange');
+
+console.log(marks);
+console.log(fruits);
+console.log(mixed);
+console.log(arr);
