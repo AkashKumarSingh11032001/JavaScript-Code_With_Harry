796755825987ce832b3fba41c82e51f20ffbc90f
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-06 22:13:05 +0530

    Day6

--- JS/app.js
+++ JS/app.js
@@ -19,3 +19,15 @@ for(var i in person) {
 console.log( i + " = " + person[i]); 
 }
 
+// For each loop
+const  alpha = ['a', 'b', 'c'];
+for (let i = 0; i < alpha.length; i++) {
+console.log(alpha[i]);
+}
+
+// While Loop
+let i = 1, n = 100;
+while (i <= n) {
+    console.log(i);
+    i += 1;
+}
