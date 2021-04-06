cc9f21dccafa8120a8208325b5a826b4d435eac6
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-06 22:08:57 +0530

    Day6

---
+++ JS/tut_8.js
@@ -0,0 +1,21 @@
+
+// let salary = 500;
+// if( salary > 1000)
+// {
+// console.log("My Salary is greater than 1000");
+// }else{
+// console.log("My Salary is less than or equal to 1000");
+// }
+
+// var salary = 500;
+// if( salary > 1000)
+// {
+// console.log("My Salary is greater than 1000");
+// }
+// else if(salary < 1000){
+// console.log("My Salary is less than 1000");
+// } else{
+// console.log("My Salary equal to 1000");
+// }
+
+// 5>0? "Greater" : "Smaller";
